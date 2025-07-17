1b76d3eefa8f62e606352d4e1ef1e94ac6721f70
chriss212 <20235787@esen.edu.sv>
2025-07-16 18:14:35 -0600

    fix: conditionally render product image in carousel

--- components/shared/product/product-carousel.tsx
+++ components/shared/product/product-carousel.tsx
@@ -32,14 +32,16 @@ const ProductCarousel = ({ data }: { data: Product[] }) => {
           <CarouselItem key={product.id}>
             <Link href={`/product/${product.slug}`}>
               <div className='relative mx-auto'>
-                <Image
-                  src={product.banner!}
-                  alt={product.name}
-                  height='0'
-                  width='0'
-                  sizes='100vw'
-                  className='w-full h-auto'
-                />
+                {product.banner ? (
+                  <Image
+                    src={product.banner!}
+                    alt={product.name}
+                    height={0}
+                    width={0}
+                    sizes='100vw'
+                    className='w-full h-auto'
+                  />
+                ) : null}
                 <div className='absolute inset-0 flex items-end justify-center'>
                   <h2 className='bg-gray-900 bg-opacity-50 text-2xl font-bold px-2 text-white'>
                     {product.name}
